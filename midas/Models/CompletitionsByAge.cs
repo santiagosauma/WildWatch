87cf309ffaa68369c5b8f8c4fbafd58c41803482
David Balleza Ayala <davidballezaa@gmail.com>
2024-04-12 14:43:06 -0600

    Add CompletitionsByAge class Model

---
+++ midas/Models/CompletitionsByAge.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace midas.Models
+{
+    public class CompletitionsByAge
+    {
+        public string Range { get; set; }
+        public int Completitions { get; set; }
+    }
+}
+
